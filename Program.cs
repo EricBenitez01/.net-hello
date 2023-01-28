d959a4d97dc839c95df265ee158ee359d020d59e
Eric Leonel Benitez <leonetkd@gmail.com>
2023-01-28 15:25:18 -0300

    hello world

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/*Console.WriteLine("introduce tu nombre");
+string Nombre = Console.ReadLine();
+
+
+Console.WriteLine("Hola, " + Nombre);*/
+
+Console.WriteLine("introduce la hora");
+int time = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Son las, " + time + " pm");
