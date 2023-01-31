2672c8bfe4103e96e3ce41e24cf641d293cf9546
Eric Leonel Benitez <leonetkd@gmail.com>
2023-01-31 10:39:08 -0300

    do while

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("introduce la hora");
 int time = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("Son las, " + time + " pm");*/
+Console.WriteLine("Son las, " + time + " pm");
 Cliente Roberto = new Cliente("roberto", 1150213296, "moreno 50", "leonetkd@gmail.com");
 Roberto.Escribe();
 
@@ -40,3 +40,38 @@ public struct Cliente
 }
 
 
+Escribe una tabla de multiplicar del 1 al 10 para un número entero que recibe por consola. Es decir, un programa que presente para el 1:
+
+
+
+int j = 1;
+int i = 0;
+Console.WriteLine("Escribe un numero para ser multiplicado");
+i = Convert.ToInt32(Console.ReadLine());
+
+
+while (j <= 10) 
+{
+    Console.WriteLine(j * i + "este es el resultado");
+    j++;
+}
+
+
+Console.WriteLine("Escribe un numero");
+int numero= Convert.ToInt32(Console.ReadLine());
+
+
+do
+{
+    if (numero>0)
+    {
+        Console.WriteLine("este numero es positivo");
+        
+    }
+    else if (numero<0)
+    {
+        
+        Console.WriteLine("Este numero es negativo");
+    }
+    break;
+} while (numero < 0 || numero > 0);*/
